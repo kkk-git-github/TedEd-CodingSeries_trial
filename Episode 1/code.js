8e8d11fba5263b705902fd4143424a5425c0a1bf
Oscar Nguyen <97220895+kkk-git-github@users.noreply.github.com>
2022-11-21 20:40:02 +0700

    Update code.js

--- Episode 1/code.js
+++ Episode 1/code.js
@@ -1,40 +1,32 @@
 /* eslint-disable require-jsdoc */
 
 class Lock {
-  value = this.value;
+  constructor() {
+    value = this.value;
+  }
 }
 
 // I would decrease the number of lock positions to 10
 // I would change the colors to true and false
-Lock1 = new Lock();
-Lock1.value = 'false';
+Lock1 = new Lock('false');
 
-Lock2 = new Lock();
-Lock2.value = 'false';
+Lock2 = new Lock('false');
 
-Lock3 = new Lock();
-Lock3.value = 'false';
+Lock3 = new Lock('false');
 
-Lock4 = new Lock();
-Lock4.value = 'false';
+Lock4 = new Lock('false');
 
-Lock5 = new Lock();
-Lock5.value = 'false';
+Lock5 = new Lock('false');
 
-Lock6 = new Lock();
-Lock6.value = 'false';
+Lock6 = new Lock('false');
 
-Lock7 = new Lock();
-Lock7.value = 'false';
+Lock7 = new Lock('false');
 
-Lock8 = new Lock();
-Lock8.value = 'false';
+Lock8 = new Lock('false');
 
-Lock9 = new Lock();
-Lock9.value = 'false';
+Lock9 = new Lock('false');
 
-Lock10 = new Lock();
-Lock10.value = 'true';
+Lock10 = new Lock('true');
 
 function breakLock() {
   // Check if Lockn.value is true
